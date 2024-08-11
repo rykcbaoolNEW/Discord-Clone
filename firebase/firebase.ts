bcd14dd2e0d428e9e38e9bdba6030224b54b35c2
rykcbaoolNEW <165583582+rykcbaoolNEW@users.noreply.github.com>
2024-08-11 16:04:49 -0400

    Update firebase.ts

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -6,12 +6,14 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCxp10smEbgwbOFA4rfA2wXV0J0H9VyLlk",
+  authDomain: "slideshowschat.firebaseapp.com",
+  databaseURL: "https://slideshowschat-default-rtdb.firebaseio.com",
+  projectId: "slideshowschat",
+  storageBucket: "slideshowschat.appspot.com",
+  messagingSenderId: "432096531830",
+  appId: "1:432096531830:web:28e47746c2300bafb8d657",
+  measurementId: "G-EC28WRKBXL"
 };
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
